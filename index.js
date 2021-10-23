24f3bf31f56ee93c9d518808acc30d2e54351f64
Tolik22869 <ggamer556@mail.ru>
2021-10-23 09:42:17 +0300

    add basic user

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const cardRoutes = require('./routes/card');
 const coursesRoutes = require('./routes/courses');
 const app = express();
 const Handlebars = require('handlebars');
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const User = require('./models/user');
 
 const hbs = exphbs.create({
     defaultLayout: 'main',
@@ -19,6 +20,16 @@ const hbs = exphbs.create({
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
+
+app.use(async (req, res, next) => {
+    try {
+        const user = await User.findById('6173a6552bd0797f06048ad9');
+        req.user = user;
+        next();
+    } catch(e) {
+        console.log(e);
+    }
+})
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
@@ -32,7 +43,20 @@ const PORT = process.env.PORT || 3000;
 const start = async () => {
     try {
         const url = `mongodb+srv://tolik:ggamer@cluster0.ncpjg.mongodb.net/shop`
-        await mongoose.connect(url, {useNewUrlParser: true});
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+        });
+
+        const candidate = await User.findOne()
+        if(!candidate) {
+            const user = new User({
+                email: 'tolik@mail.ru',
+                name: 'Tolik',
+                cart: { items: []}
+            })
+            await user.save()
+        }
+
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
--- routes/add.js
+++ routes/add.js
@@ -10,11 +10,11 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    const {title, price, img} = req.body;
     const course = new Course({
-        title:title,
-        price:price,
-        img:img
+        title: req.body.title,
+        price: req.body.price,
+        img: req.body.img,
+        userId: req.user._id
     })
 
     try {
