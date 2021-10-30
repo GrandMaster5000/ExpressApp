e4b5c5e2c84c2d87b6a7c74c9b02ac81131e5f91
Tolik22869 <ggamer556@mail.ru>
2021-10-30 10:27:58 +0300

    add frame Login page

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const addRoutes = require('./routes/add');
 const cardRoutes = require('./routes/card');
 const coursesRoutes = require('./routes/courses');
 const orderRoutes = require('./routes/orders')
+const authRoutes = require('./routes/auth')
 const app = express();
 const Handlebars = require('handlebars');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
@@ -39,6 +40,7 @@ app.use('/add' , addRoutes);
 app.use('/courses' , coursesRoutes);
 app.use('/card' , cardRoutes);
 app.use('/orders', orderRoutes)
+app.use('/auth', authRoutes)
 
 const PORT = process.env.PORT || 3000;
 
--- public/app.js
+++ public/app.js
@@ -5,12 +5,27 @@ const toCurrency = price => {
     }).format(price);
 }
 
+const toDate = date => {
+    return new Intl.DateTimeFormat('ru-RU', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    }).format(new Date(date))
+}
+
 document.querySelectorAll('.price').forEach(node => {
     node.textContent =  toCurrency(node.textContent)
 });
 
+document.querySelectorAll('.date').forEach(node => {
+    node.textContent = toDate(node.textContent)
+})
+
 const $card = document.querySelector('#card');
-if(card) {
+if($card) {
     $card.addEventListener('click', e => {
         if(e.target.classList.contains('js-remove')) {
             const id = e.target.dataset.id
@@ -40,3 +55,5 @@ if(card) {
         }  
     })
 }
+
+M.Tabs.init(document.querySelectorAll('.tabs'))
